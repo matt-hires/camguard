cdc03ab9d6791c661c3bd98032d6cc2623da4fdb
matt-hires <23478257+matt-hires@users.noreply.github.com>
2021-08-22 16:01:57 +0200

    * testing with led output

--- src/camguard/raspi_gpio_sensor.py
+++ src/camguard/raspi_gpio_sensor.py
@@ -1,7 +1,7 @@
 import logging
 from typing import Callable
 
-from gpiozero import MotionSensor as GPIOMotionSensor  # type: ignore
+from gpiozero import LED, MotionSensor as GPIOMotionSensor  # type: ignore
 
 from .bridge_impl import MotionDetectorImpl
 from .settings import RaspiGpioSensorSettings
@@ -15,12 +15,16 @@ class RaspiGpioSensor(MotionDetectorImpl):
 
     def __init__(self, settings: RaspiGpioSensorSettings) -> None:
         LOGGER.info(f"Using motion sensor on pin {settings.gpio_pin_number}")
+        # self._motion_sensor = GPIOMotionSensor(settings.gpio_pin_number, queue_len=30, sample_rate=20)
         self._motion_sensor = GPIOMotionSensor(settings.gpio_pin_number)
+        self._motion_sensor.when_activated = self._when_activated
+        self._motion_sensor.when_deactivated = self._when_deactivated
+        self._led = LED(16)
         self._settings = settings
 
     def register_handler(self, handler: Callable[..., None]) -> None:
         LOGGER.debug(f"Registering motion_sensor callback")
-        self._motion_sensor.when_activated = handler
+        self._handler = handler
 
     def shutdown(self) -> None:
         LOGGER.info(f"Shutting down")
@@ -30,3 +34,13 @@ class RaspiGpioSensor(MotionDetectorImpl):
     @property
     def id(self) -> int:
         return self._settings.gpio_pin_number
+
+    def _when_activated(self) -> None:
+       LOGGER.info(f"Sensor activated, active time: {self._motion_sensor.active_time}")
+       self._led.on()
+       if self._handler: 
+           self._handler()
+
+    def _when_deactivated(self) -> None:
+       LOGGER.info(f"Sensor activated, inactive time: {self._motion_sensor.active_time}")
+       self._led.off()
